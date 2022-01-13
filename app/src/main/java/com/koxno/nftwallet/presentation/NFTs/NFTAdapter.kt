d078faf290fb03a69030522ed62a4fb85ef4cb75
TheRealKoxno <ilya.kokhnover@gmail.com>
2022-01-14 01:27:34 +0300

    NFTadapter + NFTitem

---
+++ app/src/main/java/com/koxno/nftwallet/presentation/NFTs/NFTAdapter.kt
@@ -0,0 +1,42 @@
+package com.koxno.nftwallet.presentation.NFTs
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.koxno.nftwallet.databinding.ItemNftBinding
+import com.koxno.nftwallet.domain.entity.NFT
+import com.koxno.nftwallet.presentation.common.setImageUrl
+
+class NFTAdapter(
+    private val onNFTClicked: (NFT) -> Unit
+) : ListAdapter<NFT, NFTAdapter.ViewHolder>(
+    object : DiffUtil.ItemCallback<NFT>() {
+        override fun areItemsTheSame(oldItem: NFT, newItem: NFT): Boolean =
+            oldItem.name == newItem.name
+
+        override fun areContentsTheSame(oldItem: NFT, newItem: NFT): Boolean = oldItem == newItem
+
+    }
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            ItemNftBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder.binding) {
+            val item = getItem(position)
+            itemNftImage.setImageUrl(item.image)
+            root.setOnClickListener { onNFTClicked(item) }
+        }
+    }
+
+    class ViewHolder(val binding: ItemNftBinding) : RecyclerView.ViewHolder(binding.root)
+}
--- app/src/main/java/com/koxno/nftwallet/presentation/NFTs/NFTFragment.kt
+++ app/src/main/java/com/koxno/nftwallet/presentation/NFTs/NFTFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.koxno.nftwallet.R
 import com.koxno.nftwallet.databinding.NftsListBinding
@@ -12,6 +13,7 @@ import com.koxno.nftwallet.presentation.common.BaseFragment
 class NFTFragment : BaseFragment(R.layout.nfts_list){
 
     private val viewBinding by viewBinding(NftsListBinding::bind)
+    private val viewModel by viewModels<NFTViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
